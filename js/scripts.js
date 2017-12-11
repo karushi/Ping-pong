e59cd899483706dd259d2a3344645ea0d9c5ed3b
karushi <karuship@gmail.com>
2017-12-11 14:56:43 +0300

    add add paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
+$("#ping-pong").submit(function (event) {
+   // $("#output ul").empty();
+   var number = parseInt($("input#Number").val());
+   console.log(number);
+   for (var index = 1; index <= number; index += 1) {
+       if (index % 15 == 0) {
+           $("#output ul").append('<li>' + "PingPong" + '</li>');
+           console.log("pingpong");
+       }
+       else if (index % 5 == 0) {
+           $("#output ul").append('<li>' + "Pong" + '</li>');
+       }
+        else if (index % 3 == 0) {
+           $("#output ul").append('<li>' + "Ping" + '</li>');
+       }
+        else {
+           $("#output ul").append('<li>' + index + '</li>');
+       }
+   }
+  // $("form.number")[0].reset();
 
-$("# ping-pong").submit(function (event) {
- $("#output ul").empty();
- var number = parseInt($("input").val());
- for (var index = 1; index <= number; index += 1) {
-     if (index % 15 == 0) {
-         $("#output ul").append('<li>' + "PingPong" + '</li>');
-     }
-     else if (index % 5 == 0) {
-         $("#output ul").append('<li>' + "Pong" + '</li>');
-     }
-      else if (index % 3 == 0) {
-         $("#output ul").append('<li>' + "Ping" + '</li>');
-     }
-      else {
-         $("#output ul").append('<li>' + index + '</li>');
-     }
- }
- $("form.ping")[0].reset();
-
- event.preventDefault();
+   event.preventDefault();
 });
